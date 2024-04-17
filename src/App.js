c2f8fad2bdb15d533ed8bdbc2242cf492731dbcd
Tanushree227 <tanushreedas813@gmail.com>
2024-04-17 21:55:46 +0530

    tasks for capstone project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ function App() {
   return (
     <div>
       <h1>Capstone Project</h1>
+      <ul>
+        <li>To build navigation bar</li>
+        <li>To have heading element</li>
+        <li>To build a reservation form</li>
+        <li>to do api calling</li>
+      </ul>
     </div>
   );
 }
